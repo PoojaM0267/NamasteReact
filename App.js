93b2a088467b513acfd54775bda40a87f939760f
Pooja Mahtha <pooja0267@gmail.com>
2023-08-31 22:56:59 -0400

    Episode 3

--- App.js
+++ App.js
@@ -1,30 +1,36 @@
 import React from "react";
-import  ReactDOM from "react-dom/client";
-
-
-        // Nested Div structure 
-
-       const parent = React.createElement("div", 
-       {id :"parent"}, 
-       React.createElement("div", {id :"child" }, 
-       React.createElement("h1", {}, "I am a H1 tag nested inside div !!!")
-       ));
-
-
-
-       // 2 siblings header tags
-       const parent2 = React.createElement("div", 
-       {id :"parent"}, 
-       React.createElement("div", {id :"child" }, 
-       [React.createElement("h1", {}, "I am a H1 tag nested inside div !!!"), 
-        React.createElement("h2", {}, "I am a H2 tag nested inside div !!!")]
-       ));
-
-        
+import  ReactDOM from "react-dom/client";       
         
         // Simple H1 tag in heading
-        
+        // creating react element using core react
         const heading = React.createElement("h1", {id : "heading"}, "Hello World from React !");
+
+        // NOte : no diff between heading and jsxHeading objects
+        // Note: JSX = HTML/XML like syntax
+
+        // creating react element using jsx 
+        const jsxHeading = (<h1 id="heading" className="head">
+                Hello Namaste React using react element</h1>);
+
+
+        const Title = () => (<h1 id="heading" className="head">
+        Hello Namaste React using title component </h1>);
+
+
+
+        // React Functional component
+        const HeadingComponent = () => (
+                <div id="container">
+                        <Title />
+                        {jsxHeading}
+                        <h1>Namaste React from Functional component</h1>
+                </div>
+                
+        )
+
+
+
         const root = ReactDOM.createRoot(document.getElementById("root"));
 
-        root.render(parent2);
+       // root.render(jsxHeading);
+       root.render(<HeadingComponent />);
