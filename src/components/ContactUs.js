f21900fb4fd04992eabb81459a772143dd972c92
Pooja Mahtha <pooja0267@gmail.com>
2023-10-03 00:50:33 -0400

    unit testing part - 1

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+    <div>
+      <h1 className="font-bold text-3xl p-4 m-4">ContactUs</h1>
+
+    <form className=''>
+      <input type='text' className='border border-black p-2 m-2' placeholder='name' />
+      <input type='text' className='border border-black p-2 m-2' placeholder='message' />
+
+      <button className='border border-black p-2 m-2 rounded-lg bg-gray-100'>Submit</button>
+    </form>
+
+    </div>
   )
 }
 
---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,52 @@
+import {getAllByRole, render, screen} from '@testing-library/react'
+import ContactUs from "../ContactUs";
+import "@testing-library/jest-dom"
+
+
+// describe is just to group the test cases and 
+// they can be nested,also vcan have multiple groups
+// test === it (alias )
+
+
+describe("Contact us page test cases", () => {
+    test("Should load Contact Us Component", () => {
+        // render js component on jsdom 
+        render(<ContactUs />)
+    
+        const heading = screen.getByRole("heading")
+    
+        expect(heading).toBeInTheDocument();
+    });
+    
+    it("Should load Button inside Contact Component", () => {
+        // render js component on jsdom 
+        render(<ContactUs />)
+    
+        const btn = screen.getByRole("button")
+    
+        expect(btn).toBeInTheDocument();
+    });
+    
+    test("Should load input name inside Contact Component", () => {
+        // render js component on jsdom 
+        render(<ContactUs />)
+    
+        const inputName = screen.getByPlaceholderText("name")
+    
+        expect(inputName).toBeInTheDocument();
+    });
+    
+    test("Should load 2 input boxes on the Contact Component", () => {
+        render(<ContactUs />);
+    
+        // Query
+        const inputBoxes = screen.getAllByRole("textbox");
+    
+        expect(inputBoxes.length).toBe(2);
+    
+    });
+
+})
+
+
+
